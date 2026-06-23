4f0dd5dd57d01d589e68f07ae0cab04201c7b5a6
iamevil1335 <nilava_ug_23@ece.nits.ac.in>
2026-06-23 23:47:54 +0530

    Easy

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+         int n=gain.size();
+        vector<int> alt(n+1,0);
+        for(int i=1;i<=n;i++)
+        {
+            alt[i]=alt[i-1]+gain[i-1];
+        }
+        return *max_element(alt.begin(), alt.end());
+    }
+};
